7b93646f6fb63bc2aae57494423587fc5b0280da
salman-ibne-shofi <salmanibnshafi@gmail.com>
2024-04-03 15:51:38 +0600

    recap search and show conditional show all button

--- js/phone.js
+++ js/phone.js
@@ -8,14 +8,25 @@ const loadPhone = async (searchText) => {
 	displayPhones(phones);
 };
 const displayPhones = (phones) => {
-	// console.log(phones);
+	console.log(phones);
 
 	const phoneContainer = document.getElementById("phone-container");
-    // clear phone container cards before adding new cards
-    phoneContainer.textContent = "";
+	// clear phone container cards before adding new cards
+	phoneContainer.textContent = "";
+
+	// display show all button if there are more than 12 phones
+	const showAllContainer = document.getElementById("show-all-container");
+	if (phones.length > 12) {
+		showAllContainer.classList("hidden");
+	} else {
+		showAllContainer.classList.add("hidden");
+	}
+
+	// display only first 12 phones
+	phones = phones.slice(0, 12);
 
 	phones.forEach((phone) => {
-		console.log(phone);
+		// console.log(phone);
 		// 2 creat a div
 		const phoneCard = document.createElement("div");
 		phoneCard.classList = `card bg-gray-100 p-4 shadow-xl`;
@@ -40,6 +51,13 @@ const displayPhones = (phones) => {
 // handle search button
 const handleSearch = () => {
 	const searchField = document.getElementById("search-field");
+	const searchText = searchField.value;
+	console.log(searchText);
+	loadPhone(searchText);
+};
+
+const handleSearch2 = () => {
+	const searchField = document.getElementById("search-field2");
 	const searchText = searchField.value;
 	console.log(searchText);
 	loadPhone(searchText);
